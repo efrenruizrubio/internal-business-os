c2bfb4c0fe9bc3368f9b602f5142e37173882510
Efrén Ruíz <88808621+efrenruizrubio@users.noreply.github.com>
2026-05-10 22:11:10 -0600

    feat: implement get endpoint to retrieve projects (#57)
    * feat: implement get endpoint to retrieve projects
    
    * fix: assign correct http code for get endpoint
    
    ---------
    
    Co-authored-by: efrenruizrubio <efren.ruiz@simplebusiness.com.mx>

--- apps/server/src/modules/project/project.controller.ts
+++ apps/server/src/modules/project/project.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode, Post, Req } from '@nestjs/common'
 import { ProjectService } from './project.service'
 import { CreateProjectDto } from './project.dto'
 import { Roles } from '@/decorators/roles.decorator'
 import { UserRole } from '@/generated/prisma/enums'
+import type { AuthenticatedRequest } from '../auth/auth.types'
 
 @Controller('project')
 export class ProjectController {
@@ -13,5 +14,11 @@ export class ProjectController {
   @Post()
   async create(@Body() payload: CreateProjectDto) {
     return await this.projectService.create(payload)
+  }
+
+  @HttpCode(200)
+  @Get()
+  async get(@Req() request: AuthenticatedRequest) {
+    return await this.projectService.get(request.user)
   }
 }
